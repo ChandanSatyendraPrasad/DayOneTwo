6105a87904e328e35c4499e2da59d437794c0e83
91898 <chandansatyendraprasad@gmail.com>
2024-08-29 20:19:18 +0530

    Initial commit

---
+++ app/src/main/java/chandan/prasad/dayonetwo/Main.kt
@@ -0,0 +1,18 @@
+package chandan.prasad.dayonetwo
+fun main()
+{
+    println("HI I'M SUM")
+  /*  val a = 20
+    val b=30
+    val c=a+b
+    print("SUM= $c")*/
+
+    println("Enter first number : ")
+    val x = readln().toDouble()
+    println("First number : $x")
+    println("Enter the Second Number :")
+    val xx = readLine()!!.toInt()
+    println("Second Number : $xx")
+   
+
+}
